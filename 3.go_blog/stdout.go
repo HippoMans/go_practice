53f70c8a8b6a7ff46f40982f53b759394f4e834e
HippoMans <army1703@pukyong.ac.kr>
2020-03-20 00:23:52 +0900

    update stdout.go

--- 3.go_blog/stdout.go
+++ 3.go_blog/stdout.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	mybuffer := ""
 	arg := os.Args
+	count := len(arg)
 	if len(arg) == 1 {
 		mybuffer += "커맨드라인 인자 수를 입력하세요~~~"
 	} else {
-		mybuffer += arg[1:]
+		for i := 1; i < count; i++ {
+			mybuffer += arg[i]
+		}
 	}
 
 	io.WriteString(os.Stdout, mybuffer)
